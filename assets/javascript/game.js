a1158ded942146755aa06ac99fb0b2d3e95e8c7b
Gue <guehyang@gmail.com>
2019-06-26 02:00:20 -0700

    Added Bootstrap purple button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
-var winCounter = 0;
-var remainingGuesses = 13;
-var Guessed = [];
-
-var wrongGuess = [];
-var rightGuess = [];
+//Load Javascript after the DOM has been loaded. If this is working, you'd see 'Live reload enabled.' in the console.
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //Initiatizing variables
+    var word = ["container","container-fluid", "row", "col",]
+    var winCounter = 0;
+    var loseCounter = 0;
+    var remainingGuess = 13;
+    var Guessed = [];
+    var wrongGuess = [];
+    var rightGuess = [];
+  });
