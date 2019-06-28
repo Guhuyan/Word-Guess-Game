c52852e322dee18abc102069d9fb8b29c84d4df9
Gue <guehyang@gmail.com>
2019-06-28 01:45:20 -0700

    It is done!  Just need to add js to button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,59 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     //Array with all the words
     const words = ["container","container-fluid", "row", "row-no-gutters", "col","active", "danger", "info", "success", "warning", "affix", "alert", "alert-dismissible", "alert-link", "breadcrumb", "btn", "btn-block", "btn-group", "btn-group-justified", "btn-group-vertical", "btn-info", "btn-link", "btn-success", "btn-warning", "caption", "caret", "carousel", "carousel-caption", "carousel-control", "carousel-indicators", "carousel-inner", "item", "left", "right", "next", "prev", "center-block", "checkbox", "checkbox-inline", "clearfix", "close", "collapse", "control-label", "divider", "dropdown", "dropdown-header", "dropdown-menu", "dropdown-menu-right", "embed-responsive", "embed-responsive-item", "dropdown-toggle", "form-control", "form-group", "form-horizontal", "glyphicon", "has-danger", "has-success", "had-warning", "help-block", "hidden", "show", "invisible", "icon-bar", "icon-next", "icon-prev", "img-circle", "img-responsive", "img-rounded", "img-thumbnail", "in", "input-group", "input-group-addon", "input-group-btn", "jumbotron", "label", "label-danger", "label-info", "label-success", "label-warning", "lead", "list-group", "list-group-item", "list-group-item-heading", "item-group-item-text", "list-inline", "list-unstyled", "mark", "media", "media-body", "media-left", "media-right", "media-heading", "media-list", "media-object", "modal", "modal-body", "modal-content", "modal-dialog", "modal-footer", "modal-header", "modal-lg", "modal-open", "modal-sm", "modal-title", "nav", "nav-justified", "nav-stacked", "nav-tabs", "navbar", "navbar-brand", "navbar-btn", "navbar-collapse", "navbar-fixed-bottom", "navbar-fixed-top", "navbar-form", "navbar-header", "navbar-default", "navbar-inverse", "navbar-left", "navbar-right", "navbar-link", "navbar-text", "navbar-toggle", "navbar-nav", "nav-tabs", "nav-pills", "page-header", "pager", "pagination", "pagination-lg", "pagination-sm", "previous", "next", "panel", "panel-group", "panel-body", "panel-collapse", "panel-title", "panel-heading", "panel-footer", "popover", "pre-scrollable", "progress", "progress-bar", "progress-bar-striped", "pull-left", "pull-right", "small", "sr-only", "sr-only-focusable", "tab-content", "tab-pane", "table", "table-bordered", "table-condensed", "table-hover", "table-responsive", "text-uppercase", "text-lowercase", "text-hide", "text-muted", "text-center", "text-left", "text-right", "text-justify", "text-nowrap", "thumbnail", "tooltip", "well", "well-lg", "well-sm"];
-    //Initializing variable to six (i. e. head, body, left arm, right arm, left leg, and right leg)
-    let remainingGuess = 7;
+    //Create an array to hold characters adding up to the word, but in array form.
+    let correctWord = [];
+    //Create an empty array to display to the user the active word in a progressive manner
+    let displayWord = [];
     //Creating an empty array to hold the letters the player have guessed
     let displayGuess = [];
+    //Initializing variable to six (i. e. head, body, left arm, right arm, left leg, and right leg)
+    let remainingGuess = 7;
     //Setting initial value for the win and lose counters
     let winCounter = 0;
     let loseCounter = 0;
-
     //Computer pick a random word
     let chosenWord = words[Math.floor(Math.random() * words.length)];
     console.log(chosenWord);
 
-    //Create an array and initialize it with underscore in each index, then pass it to #underScores in index.html
-    let displayWord = [];
+    //Function to reset all global variables
+    function resetVar() {
+      correctWord = [];
+      displayWord = [];
+      displayGuess = [];
+      remainingGuess = 7;
+      chosenWord = words[Math.floor(Math.random() * words.length)];
+    }
+
+    function runGame() {
+      //Initialize the displayWord array with underscore in each index, then pass it to #underScores in index.html
+      for (let i = 0; i < chosenWord.length; i++) {
+      displayWord[i] = "_";
+      }
+      document.getElementById("underScores").textContent = displayWord.join(" ");
+
+      //Create an array to hold all the letters from the chosen word in its own index
+      correctWord = [];
+      for (let r = 0; r < chosenWord.length; r++) {
+        correctWord.push(chosenWord.charAt(r));
+      }
+      console.log(chosenWord);
+      //Clearing the characters that have already been guessed and replace it with its default text.
+      document.getElementById("guessedChar").textContent = "Characters Already Guessed";
+    }
+
+    //Initialize the displayWord array with underscore in each index, then pass it to #underScores in index.html
     for (let i = 0; i < chosenWord.length; i++) {
       displayWord[i] = "_";
-    }
-    document.getElementById("underScores").textContent = displayWord.join(" ");
+      }
+      document.getElementById("underScores").textContent = displayWord.join(" ");
 
-    //Create an array to hold all the letters from the chosen word in its own index
-    correctWord = [];
-    for (let r = 0; r < chosenWord.length; r++) {
-      correctWord.push(chosenWord.charAt(r));
-    }
-    console.log(correctWord);
+      //Create an array to hold all the letters from the chosen word in its own index
+      correctWord = [];
+      for (let r = 0; r < chosenWord.length; r++) {
+        correctWord.push(chosenWord.charAt(r));
+      }
+      console.log(correctWord);
 
     //Adding event listener to listen for a keypress.  Once the key is pressed, record the key as a string into a variable called key.
     document.addEventListener("keypress", (event) => {
@@ -51,12 +79,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
           loseCounter++;
           document.getElementById("loseTracker").textContent = loseCounter;
           alert("You lose!");
+          resetVar();
+          runGame();
         }
       }
       if (JSON.stringify(displayWord) === JSON.stringify(correctWord)) {
         winCounter++;
         document.getElementById("winTracker").textContent = winCounter;
         alert("You win!");
+        resetVar();
+        runGame();
       }
     });
   });
